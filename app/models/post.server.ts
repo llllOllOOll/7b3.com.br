c8b3bcfda1426a4887bd99b81a54eb0a01889bd5
seven <7b37b3@gmail.com>
2023-02-01 11:01:04 -0300

    official blog tutorial from Remix

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,11 +1,14 @@
 import { prisma } from "~/db.server";
+import type { Post } from "@prisma/client";
 
 export async function getPosts() {
   return prisma.post.findMany();
 }
 
-type Post = {
-  slug: string;
-  title: string;
-};
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
 
+export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">) {
+  return prisma.post.create({ data: post });
+}
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -3,7 +3,7 @@ import { Link, useLoaderData } from "@remix-run/react";
 import { getPosts } from "~/models/post.server";
 
 export const loader = async () => {
-     return json({ posts: await getPosts() })
+  return json({ posts: await getPosts() });
 };
 
 export default function Posts() {
@@ -12,19 +12,19 @@ export default function Posts() {
   return (
     <main>
       <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
+
       <ul>
-        {posts.map((post:any) => (
+        {posts.map((post: any) => (
           <li key={post.slug}>
-            <Link
-              to={post.slug}
-              className="text-blue-600 underline"
-            >
+            <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
             </Link>
           </li>
         ))}
       </ul>
-
     </main>
   );
 }
