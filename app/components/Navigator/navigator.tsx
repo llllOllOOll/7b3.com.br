47572d55511a657fd9812ffe7e2cdcc4f6906706
seven <7b37b3@gmail.com>
2023-02-01 17:05:23 -0300

    estrutura principal do site criada

---
+++ app/components/Navigator/navigator.tsx
@@ -0,0 +1,32 @@
+export default function Navigator() {
+  return (
+    <header className="flex items-center justify-center bg-blog-black-700">
+      <nav className="flex w-full max-w-6xl items-center justify-around  py-4">
+        <div>
+          <img src="images/logo-menu.svg" />
+        </div>
+
+        <div>
+          <ul className="flex space-x-16 text-blog-grey-200">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Artigos</a>
+            </li>
+            <li>
+              <a href="#">Vídeos</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <img src="images/hamburguer-menu.svg" />
+        </div>
+      </nav>
+    </header>
+  );
+}
