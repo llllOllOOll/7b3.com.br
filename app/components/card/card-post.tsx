6c0a59765a1bcfd79c48858730726db0009d1c0b
seven <7b37b3@gmail.com>
2023-02-03 18:24:09 -0300

    section posts recente and videos recentes added

---
+++ app/components/card/card-post.tsx
@@ -0,0 +1,22 @@
+type CardProps = {
+  title: string;
+  author: string;
+  date: string;
+};
+
+export default function Card({ title, author, date }: CardProps) {
+  return (
+    <div className="w-full space-y-3 rounded-lg bg-gray-100 p-6 shadow-md">
+      <div className="h-64 max-w-lg bg-gray-500"></div>
+      <p>Fonte</p>
+      <h2 className="text-center text-lg">{title}</h2>
+      <div className="flex items-center space-x-3 text-sm">
+        <div className="h-10 w-10 rounded-full bg-slate-600"></div>
+        <div>
+          <p>{author}</p>
+          <p>{date}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import Card from "~/components/card/card-post";
 import Navigator from "~/components/navigator/navigator";
 
 export default function Index() {
@@ -30,12 +31,30 @@ export default function Index() {
             </div>
           </section>
         </div>
-
-        <section className="mt-20 mb-20">
-          <h2>POSTS RECENTES</h2>
+        <section className="w-full bg-slate-200 py-16">
+          <h2 className=" text-center">POSTS RECENTES</h2>
+          <div className="mt-24  flex gap-8 px-96">
+            <Card
+              title={"Pai e filha brigam por codigo"}
+              author={"Luna Miranda"}
+              date={"March, 14"}
+            />
+            <Card
+              title={"Pai e filha brigam por codigo"}
+              author={"Luna Miranda"}
+              date={"March, 14"}
+            />
+          </div>
+        </section>
+        <section className="mb-20  w-full bg-white">
+          <h2 className="mt-20 text-center">VIDEO RECENTE</h2>
           <div>
-            <div></div>
-            <div></div>
+            <div>
+<iframe width="1904" height="810" src="https://www.youtube.com/embed/tbcveXCPqCA" title="Explorando o Remix e os Padrões da Web: Aprenda a Usar a Fetch API" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+              <div>Exporando o Remix e os Padr
+              es WEB
+            </div>
           </div>
         </section>
       </main>
